52f8dcbf6d5984d903eaf65a19b99d6a1882b6a8
EricoMeger <ericomeger9@gmail.com>
2025-09-27 16:38:13 -0300

    make code more readable

--- code/mandelbrot.cpp
+++ code/mandelbrot.cpp
@@ -4,96 +4,92 @@
 #include <cmath>
 #include <ctime>
 
-int i4_min(int i1, int i2) {
-    if(i1 < i2) return i1;
-    return i2;
-}
-
 int main() {
-    int m = 1000;
-    int n = 1000;
+    int image_height = 1000;
+    int image_width = 1000;
+
+    const int max_iterations = 700000;
+    const char* output_filename = "mandelbrot.ppm";
+
+    // limites do plano complexo
+    double plane_x_min = -2.25;
+    double plane_x_max = 1.25;
+    double plane_y_min = -1.75;
+    double plane_y_max = 1.75;
+
+    // matriz de cor e iterações
+    std::vector<std::vector<int>> red(image_height, std::vector<int>(image_width));
+    std::vector<std::vector<int>> green(image_height, std::vector<int>(image_width));
+    std::vector<std::vector<int>> blue(image_height, std::vector<int>(image_width));
+    std::vector<std::vector<int>> iteration_count(image_height, std::vector<int>(image_width));
 
     timespec ts_start{}, ts_end{};
-    double time_total;
-    int c;
-    int count_max = 1000;
-
-    char const *filename = "mandelbrot.ppm";
-
-    double x_max = 1.25;
-    double x_min = -2.25;
-    double y_max = 1.75;
-    double y_min = -1.75;
-
-    std::vector<std::vector<int>> r(m, std::vector<int>(n));
-    std::vector<std::vector<int>> g(m, std::vector<int>(n));
-    std::vector<std::vector<int>> b(m, std::vector<int>(n));
-    std::vector<std::vector<int>> count(m, std::vector<int>(n));
-    
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
 
-    for (int i = 0; i < m; i++) {
-        double y = ((i - 1) * y_max + (m - i) * y_min) / (m - 1);
-        for (int j = 0; j < n; j++) {
-            double x = ((j - 1) * x_max + (n - j) * x_min) / (n - 1);
-            count[i][j] = 0;
-            double x1 = x;
-            double y1 = y;
+    // calculo do mandelbrot
+    for (int row = 0; row < image_height; row++) {
+        // mapeia a coluna de pixels para o y imaignario
+        double c_imag = plane_y_min + (row / static_cast<double>(image_height - 1)) * (plane_y_max - plane_y_min);
 
-            for (int k = 1; k <= count_max; k++) {
-                double x2 = x1 * x1 - y1 * y1 + x;
-                double y2 = 2 * x1 * y1 + y;
+        for (int col = 0; col < image_width; col++) {
+            // mapaeia a coluna de pixels para o x real
+            double c_real = plane_x_min + (col / static_cast<double>(image_width - 1)) * (plane_x_max - plane_x_min);
 
-                if (x2 < -2.0 || 2.0 < x2 || y2 < -2.0 || 2.0 < y2) {
-                    count[i][j] = k;
+            double z_real = c_real;
+            double z_imag = c_imag;
+            iteration_count[row][col] = 0;
+
+            // itera z = z^2 + c até divergir ou atingir o maximo de iterações
+            for (int k = 1; k <= max_iterations; k++) {
+                double z_real_next = z_real * z_real - z_imag * z_imag + c_real;
+                double z_imag_next = 2.0 * z_real * z_imag + c_imag;
+
+                // se o valor passar de 2 com certeza vai divergir ao infinito
+                if(std::abs(z_real_next) > 2.0 || std::abs(z_imag_next) > 2.0) {
+                    iteration_count[row][col] = k;
                     break;
                 }
-                x1 = x2;
-                y1 = y2;
+
+                z_real = z_real_next;
+                z_imag = z_imag_next;
             }
 
-            if (count[i][j] == 0) {
-                r[i][j] = 0;
-                g[i][j] = 0;
-                b[i][j] = 0;
-            } else if ((count[i][j] % 2) == 1) {
-                r[i][j] = 255;
-                g[i][j] = 255;
-                b[i][j] = 255;
+            if (iteration_count[row][col] == 0) {
+                red[row][col] = green[row][col] = blue[row][col] = 0;  // se faz parte do Mandelbrot
+            } else if (iteration_count[row][col] % 2 == 1) {
+                red[row][col] = green[row][col] = blue[row][col] = 255; // white
             } else {
-                c = static_cast<int>(255.0 * (1 - std::log(count[i][j]) / std::log(count_max)));
-                r[i][j] = c;
-                g[i][j] = c;
-                b[i][j] = c;
+                int color_value = static_cast<int>(
+                    255.0 * (1.0 - std::log(iteration_count[row][col]) / std::log(max_iterations))
+                );
+                red[row][col] = green[row][col] = blue[row][col] = color_value;
             }
         }
     }
 
     clock_gettime(CLOCK_MONOTONIC, &ts_end);
-    time_total = (ts_end.tv_sec - ts_start.tv_sec) * 1e9 + (ts_end.tv_nsec - ts_start.tv_nsec);
+    double total_time_ns = (ts_end.tv_sec - ts_start.tv_sec) * 1e9 + (ts_end.tv_nsec - ts_start.tv_nsec);
+    std::cout << "\nTotal time: " << total_time_ns / 1e6 << " ms\n";
 
-    std::cout << "\nTotal time is " << time_total / 1e6 << " ms\n";
+    std::ofstream output(output_filename);
+    // Header do PPM
+    // tipo da imagem
+    output << "P3\n";
+    // tamanho width e height
+    output << image_width << " " << image_height << "\n";
+    // valor max do pixel
+    output << "255\n";
 
-    std::ofstream output_unit(filename);
-    // para o SO entender o arquivo como imagem PPM ele espera:
-    // Tipo do arquvio
-    output_unit << "P3\n";
-    // width e height
-    output_unit << n << " " << m << "\n";
-    // valor maximo
-    output_unit << "255\n";
-
-    // R G B R G B... valor de cada pixel
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            output_unit << r[i][j] << " " << g[i][j] << " " << b[i][j];
-            if (j < n - 1) output_unit << " ";
+    for (int row = 0; row < image_height; row++) {
+        for (int col = 0; col < image_width; col++) {
+            output << red[row][col] << " " << green[row][col] << " " << blue[row][col];
+            if (col < image_width - 1) output << " ";
         }
-        output_unit << "\n";
+        output << "\n";
     }
 
-    output_unit.close();
-    std::cout << "\nGraphics data written to \"" << filename << "\".\n\n";
+    output.close();
+    std::cout << "\nImage written to \"" << output_filename << "\".\n\n";
 
     return 0;
 }
